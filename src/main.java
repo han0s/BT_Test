795e52b385650b89f102a9e77757afec67d8a7f9
Hannes Honkasaari <hayn0seh@gmail.com>
2013-06-11 16:30:22 +0300

    first lines

--- src/main.java
+++ src/main.java
@@ -1,12 +1,24 @@
+import java.awt.*;
 
-public class main {
+import javax.swing.*;
 
-	/**
-	 * @param args
-	 */
+
+public class main extends JFrame {
+
+	main(){
+		setTitle("Test window 0.1");
+		setLayout(new BoxLayout(getContentPane(),BoxLayout.X_AXIS));
+		setPreferredSize(new Dimension(500,500));
+		setVisible(true);
+		pack();
+	}
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("lol");
+		new main();
 
 	}
 
